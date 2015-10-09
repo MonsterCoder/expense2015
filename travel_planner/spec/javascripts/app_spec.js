427e1238e26222f4a87c444825d9a63bca51f226
monstercoder <monstercoder@gmail.com>
2015-10-08 22:03:31 -0500

    ui route

--- travel_planner/spec/javascripts/app_spec.js
+++ travel_planner/spec/javascripts/app_spec.js
@@ -1,9 +1,11 @@
 describe("travel planner app", function() {
-    var $state;
+    var $state, $rootScope;
     
     beforeEach(module("TravePlannerApp"));
-    beforeEach(inject(function(_$state_) {
+    beforeEach(inject(function(_$state_, _$rootScope_) {
         $state = _$state_;
+        $rootScope = _$rootScope_;
+        $rootScope.$apply();
     }));
     
     it(" defaults to login state", function() {
